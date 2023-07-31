fffa824e2b6f2bcf74d62693c0a2642d647f2b76
Pritam Nikam <pritam.nikam@gmail.com>
2023-07-31 10:10:17 +0530

    Modified deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,29 @@
 const hre = require("hardhat");
 
 async function main() {
-  const { arbiter, beneficiary } = await hre.ethers.getSigners();
-  const amount = hre.ethers.parseEther("0.00001");
-  const escrow = await hre.ethers.deployContract(
-    "Escrow",
-    arbiter,
-    beneficiary,
-    amount
-  );
+  const deposit = ethers.utils.parseEther('1');
+  const depositor = ethers.provider.getSigner(0);
+  const beneficiary = ethers.provider.getSigner(1);
+  const arbiter = ethers.provider.getSigner(2);
+  try {
+    const Escrow = await ethers.getContractFactory('Escrow');
+    const contract = await Escrow.deploy(
+      arbiter.getAddress(),
+      beneficiary.getAddress(),
+      {
+        value: deposit,
+      }
+    );
+    await contract.deployed();
 
-  await escrow.waitForDeployment();
-
-  console.log(
-    `Escrow with ${hre.ethers.formatEther(
-        amount
-    )}ETH deployed to ${escrow.target}`
-  );
+    console.log(
+      `Escrow with ${hre.ethers.utils.formatEther(
+        deposit
+      )}ETH deployed to ${contract.address}`
+    );
+  } catch(error) {
+    console.error(error);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
